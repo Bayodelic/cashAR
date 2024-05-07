de6fc2caddbbc3f3edfadfd4ff68c4d233673381
Ivanovicx Nuñez <ivan25606@gmail.com>
2024-05-06 23:04:24 -0600

    View Controller base

--- AR_prueba/ViewController.swift
+++ AR_prueba/ViewController.swift
@@ -5,37 +5,90 @@
 //  Created by Ivanovicx Nuñez on 30/04/24.
 //
 
+/*
+ 
+ Notas:
+ 
+    - Implementar un JSON para limpiar las etiquetas
+        "1_peso_MXN": "1 peso"
+ 
+ */
+
 import UIKit
 import SceneKit
 import ARKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    lazy var detectionRequest: VNCoreMLRequest = {
+            do {
+                let model = try VNCoreMLModel(for: Peso_1().model)
+                
+                let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                    self?.processDetections(for: request, error: error)
+                })
+                request.imageCropAndScaleOption = .scaleFill
+                return request
+            } catch {
+                fatalError("Failed to load Vision ML model: \(error)")
+            }
+        }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("View Did Load")
         
         // Set the view's delegate
         sceneView.delegate = self
         
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        
+        
     }
     
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        
+//        // Create a session configuration
+//        let configuration = ARImageTrackingConfiguration()
+//        
+//        configuration.maximumNumberOfTrackedImages = 100
+//        
+//        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "billetes", bundle: nil) else {
+//            fatalError("Couldn't load tracing images")
+//        }
+//        
+//        configuration.trackingImages = trackingImages 
+//
+//        // Run the view's session
+//        sceneView.session.run(configuration)
+//    }
+    
     override func viewWillAppear(_ animated: Bool) {
+        
+        
         super.viewWillAppear(animated)
         
+        
+        print("Will Appear")
+        
         // Create a session configuration
-//        let configuration = ARWorldTrackingConfiguration()
-        let configuration = ARImageTrackingConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
         
-        configuration.maximumNumberOfTrackedImages = 100
+        // Enable horizontal plane detection
+        configuration.planeDetection = [.horizontal]
         
-        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "billetes", bundle: nil) else {
-            fatalError("Couldn't load tracing images")
-        }
-        
-        configuration.trackingImages = trackingImages 
-
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -43,25 +96,81 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
+        print("Will Disappear")
+        
         // Pause the view's session
         sceneView.session.pause()
     }
     
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        guard let imageAnchor = anchor as? ARImageAnchor else {
-            return nil
+//    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+//        guard let imageAnchor = anchor as? ARImageAnchor else {
+//            return nil
+//        }
+//        
+//        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+//        plane.firstMaterial?.diffuse.contents = UIColor.blue
+//        
+//        let planeNode = SCNNode(geometry: plane)
+//        planeNode.eulerAngles.x = -.pi / 2
+//        
+//        let node = SCNNode()
+//        node.addChildNode(planeNode)
+//        
+//        return node
+//    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+       guard let imageAnchor = anchor as? ARImageAnchor else { return }
+       
+        
+        print("Renderer")
+        
+       // Create a plane geometry to visualize the detected image
+       let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width,
+                            height: imageAnchor.referenceImage.physicalSize.height)
+       plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.5)
+       
+       // Create a plane node and add it to the scene
+       let planeNode = SCNNode(geometry: plane)
+       planeNode.eulerAngles.x = -.pi / 2
+       node.addChildNode(planeNode)
+       }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        
+        
+        print("Session")
+        
+        let imageBuffer = frame.capturedImage
+        
+        let ciImage = CIImage(cvImageBuffer: imageBuffer)
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        
+        do {
+            try handler.perform([detectionRequest])
+        } catch {
+            print("Failed to perform detection: \(error.localizedDescription)")
+        }
+    }
+    
+    private func processDetections(for request: VNRequest, error: Error?) {
+        guard let results = request.results else {
+            print("Unable to detect anything.\n\(error!.localizedDescription)")
+            return
         }
         
-        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-        plane.firstMaterial?.diffuse.contents = UIColor.blue
         
-        let planeNode = SCNNode(geometry: plane)
-        planeNode.eulerAngles.x = -.pi / 2
+        print("Process Detention")
         
-        let node = SCNNode()
-        node.addChildNode(planeNode)
-        
-        return node
+        let detections = results as! [VNRecognizedObjectObservation]
+        // Handle the detected objects here
+        // For example, you can add 3D objects to the scene based on the detected objects
+        for detection in detections {
+            // Retrieve the identifier of the detected object
+            let identifier = detection.labels[0].identifier
+            print("Detected object: \(identifier)")
+        }
     }
 
 }
---
+++ AR_prueba/ViewControllerML.swift
@@ -0,0 +1,8 @@
+//
+//  ViewControllerML.swift
+//  AR_prueba
+//
+//  Created by Ivanovicx Nuñez on 06/05/24.
+//
+
+import Foundation
